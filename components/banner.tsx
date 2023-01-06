837d081a3bc7a9eda4ac5503917598caa16fe44c
Curtis Warcup <88569957+Cwarcup@users.noreply.github.com>
2023-01-06 14:14:40 -0800

    banner styling

--- components/banner.tsx
+++ components/banner.tsx
@@ -12,7 +12,7 @@ const Banner = (props: Props) => {
         <span className={styles.title1}>Coffee</span>
         <span className={styles.title2}>Critic</span>
       </h1>
-      <p className={styles.subtitle}>Discover your local coffee shops!</p>
+      <p className={styles.subTitle}>Discover your local coffee shops!</p>
       <button className={styles.button}>{props.buttonText}</button>
     </div>
   )
