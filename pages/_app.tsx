b33967ace56ba830e5f535de52378d7386436585
Curtis Warcup <88569957+Cwarcup@users.noreply.github.com>
2023-01-06 13:09:15 -0800

    CSS modules

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,18 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css"
+import type { AppProps } from "next/app"
+import styles from "../styles/Home.module.css"
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <footer className={styles.footer}>
+        <div className="center">
+          <p>
+            <strong>Next.js</strong> by <a href="cwarcup.com">Curtis Warcup</a>
+          </p>
+        </div>
+      </footer>
+    </>
+  )
 }
