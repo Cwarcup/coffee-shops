40766c6d7daa439410bd724b610fef1ec101b465
Curtis Warcup <88569957+Cwarcup@users.noreply.github.com>
2023-01-12 15:45:41 -0800

    error handling for getCoffeeStoresById

--- pages/api/getCoffeeStoresById.ts
+++ pages/api/getCoffeeStoresById.ts
@@ -1,22 +1,28 @@
 import { NextApiRequest, NextApiResponse } from "next"
 
-import {
-  table,
-  getMinifiedRecords,
-  findRecordByFilter,
-} from "../../lib/airtable"
+import { findRecordByFilter } from "../../lib/airtable"
 
 // The getCoffeeStoresById function is used to get the data for a specific record in the coffee-stores table.
-
 const getCoffeeStoresById = async (
   req: NextApiRequest,
   res: NextApiResponse
 ) => {
   // get the id from the query
-  const { id } = req.query
-  //
-  const coffeeStore = await findRecordByFilter(id as string)
-  res.status(200).json(coffeeStore)
+  const { id } = req.query as { id: string }
+  try {
+    const coffeeStore = await findRecordByFilter(id)
+
+    // if the record exists, return the record
+    if (coffeeStore.length !== 0) {
+      return res.json(coffeeStore)
+    } else {
+      return res
+        .status(404)
+        .json({ message: `Record not found with id: ${id}` })
+    }
+  } catch (error) {
+    res.status(500).json({ msg: "Something went wrong" })
+  }
 }
 
 export default getCoffeeStoresById
